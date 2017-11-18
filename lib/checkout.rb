84d7e0181ce3a2960471097439a20837ec28651f
Arthur Granado <agranado@pluga.co>
2017-11-18 18:18:05 -0200

    Refactory to group items with same sku

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,15 +4,24 @@ module Syft
 
     def initialize(pr = [])
       @promotion_rules = pr;
-      @items = []
+      @items = {}
     end
 
     def scan(item)
-      @items << item
+      @items[item.sku] = (@items.fetch(item.sku, []) << item)
     end
 
     def total
-      total_price = items.reduce(0){|s,i| s += i.price; s}
+      total_price = 0
+      items.each do |sku, item_list|
+        total_price += item_list.reduce(0){|s,i| s += i.price; s}
+      end
+
+      item_rules = @promotion_rules.select{|r| r.type == :item}
+
+      #item_rules.each do |rule|
+        
+      #end
 
       rule = @promotion_rules.first
       total_price *= (1.0-rule.discount) if !rule.nil?  && total_price >= rule.value
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,9 +3,9 @@ require "rspec"
 require_relative "../lib/checkout"
 
 describe Syft::Checkout do
-  let(:item1) {double("Lavender heart", price: 9.25)}
-  let(:item2) {double("Personalised Cufflinks", price: 45)}
-  let(:item3) {double("Kids T-shirt", price: 19.95)}
+  let(:item1) {double("Lavender heart", price: 9.25 , sku: "s01")}
+  let(:item2) {double("Personalised Cufflinks", price: 45, sku: "s02")}
+  let(:item3) {double("Kids T-shirt", price: 19.95, sku: "s03")}
 
   it "should calculate total price 0 from empty checkout" do
     expect(subject.total).to eq(0)
@@ -14,26 +14,19 @@ describe Syft::Checkout do
   describe "Checkout with itens" do
     describe "Without Promotion Rules" do
       before(:each) do
-        item1 = double("Item 1", price: 5)
-        item2 = double("Item 2", price: 6)
-
         subject.scan(item1)
         subject.scan(item2)
       end
 
       it "should calculate total price from scanned itens" do
-        expect(subject.total).to eq(11)
+        expect(subject.total).to eq(54.25)
       end
     end
 
     describe "With Promotion Rules" do
-      let(:rule_1_1) {double("Rule 1.1: price over 60 pounds give 10% discount",
-                             type: "discount", value: 60, discount: 0.1)}
-      let(:rule_1_2) {double("Rule 1.2: price over 50 pounds give 15% discount",
-                             type: "discount", value: 50, discount: 0.15)}
-      let(:rule_2) {double("Rule 2: 2 Lavender hearts price drops to 8.50")}
-
       describe "Promotion Rule 1.1" do
+        let(:rule_1_1) {double("Rule 1.1: price over 60 pounds give 10% discount",
+                               type: "discount", value: 60, discount: 0.1)}
         subject { Syft::Checkout.new([rule_1_1]) }
 
         describe "Checkout total over 60" do
@@ -61,6 +54,8 @@ describe Syft::Checkout do
       end
 
       describe "Promotion Rule 1.2" do
+        let(:rule_1_2) {double("Rule 1.2: price over 50 pounds give 15% discount",
+                               type: "discount", value: 50, discount: 0.15)}
         subject { Syft::Checkout.new([rule_1_2]) }
 
         describe "Checkout total over 60" do
@@ -71,6 +66,37 @@ describe Syft::Checkout do
 
           it "should give discount" do
             expect(subject.total).to eq(46.11)
+          end
+        end
+
+      end
+
+      xdescribe "Promotion Rule 2.1" do
+        let(:rule_2_1) {double("Rule 2: 2 Lavender hearts price drops to 8.50",
+                               type: "item", value: 8.5, sku: "01")}
+        subject { Syft::Checkout.new([rule_2_1]) }
+
+        describe "Checkout with 2 items 1" do
+          before(:each) do
+            subject.scan(item1)
+            subject.scan(item2)
+            subject.scan(item1)
+          end
+
+          it "should give discount" do
+            expect(subject.total).to eq(36.95)
+          end
+        end
+
+        describe "Checkout total with only 1 item 1" do
+          before(:each) do
+            subject.scan(item1)
+            subject.scan(item2)
+            subject.scan(item2)
+          end
+
+          it "should not give discount" do
+            expect(subject.total).to eq(99.25)
           end
         end
 
